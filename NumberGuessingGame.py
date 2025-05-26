e586fca17ad6e4874ae5c2a80c054af96a887ff8
Angel Campos <angelcampos@MacBook-Pro.local>
2025-05-26 14:24:32 -0500

    updated to limit attempts

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,14 +1,19 @@
 import random
-secret_number = random.randint(1,100)
+secret_number = random.randint(1, 100)
 attempts = 0
+
 while True:
     guess = int(input("Guess the number (between 1 and 100): "))
     attempts += 1
 
+    if attempts == 10: 
+        print(f"Game over! The correct number was {secret_number}.")
+        break
+
     if guess < secret_number:
-        print("Too low! Try again.")
+        print(f"Too low! Try between {guess} and 100.")
     elif guess > secret_number:
-        print("Too high! Try again")
+        print(f"Too high! Try between 1 and {guess}.")
     else: 
-        print(f"Congratulations! you guessed the number in {attempts} attempts.")  
+        print(f"Congratulations! You guessed the number in {attempts} attempts.")  
         break
